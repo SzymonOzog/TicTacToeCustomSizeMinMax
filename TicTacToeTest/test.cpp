ed130392608c80f7c1e7fd823b026b95002ab6e2
Szymon Ozog <szymon.ozog@gmail.com>
2020-05-31 10:31:56 +0200

    Test fixes

--- TicTacToeTest/test.cpp
+++ TicTacToeTest/test.cpp
@@ -1,13 +1,11 @@
 #include "gtest/gtest.h"
 #include "TicTacToe.h"
-const int fieldSide = 4;
 int TicTacToe::nFieldSide;
-TicTacToe t(fieldSide);
-std::vector<player> f(fieldSide* fieldSide);
-
+int AI::nFieldSide;
 TEST(FieldTests, HasWonTest)
 {
-    Field f(5 * 5);
+    int fieldSide = 5;
+    Field f(fieldSide * fieldSide);
     for (auto p : { player::AI, player::Human })
     {
         for (int i = 0; i < fieldSide; i++)
@@ -67,36 +65,40 @@ TEST(FieldTests, isDrawTest)
 }
 //Not really a test, just checking how long will the AI think 
 //when provided with an empty field
-TEST(AITests, EmptyFieldTimeElapse)
-{
-    std::shared_ptr<Field> field = std::make_shared<Field>(4);
-    AI ai(field);
-    std::pair<int, int> choice = ai.findBestMove();
-}
+//TEST(AITests, EmptyFieldTimeElapse)
+//{
+//    std::shared_ptr<Field> field = std::make_shared<Field>(4 * 4);
+//    AI ai(field);
+//    std::pair<int, int> choice = ai.findBestMove();
+//}
 TEST(AITests, WillWin)
 {
-    std::shared_ptr<Field> field = std::make_shared<Field>(4);
-    AI ai(field);
-    (*field)[5] = player::AI;
-    (*field)[6] = player::AI;
-    (*field)[7] = player::AI;
-    (*field)[8] = player::AI;
-    std::pair<int, int> choice = ai.findBestMove();
-    EXPECT_EQ(choice.second, 9);
-}
-TEST(AITests, WillBlockPlayerWin)
-{
-    std::shared_ptr<Field> field = std::make_shared<Field>(4);
+    std::shared_ptr<Field> field = std::make_shared<Field>(4 * 4);
     AI ai(field);
     (*field)[0] = player::AI;
     (*field)[1] = player::AI;
     (*field)[2] = player::AI;
+    (*field)[8] = player::Human;
+    (*field)[4] = player::Human;
+    (*field)[7] = player::Human;
+    (*field)[15] = player::Human;
+
+    std::pair<int, int> choice = ai.findBestMove();
+    EXPECT_EQ(choice.second, 3);
+}
+TEST(AITests, WillBlockPlayerWin)
+{
+    std::shared_ptr<Field> field = std::make_shared<Field>(4 * 4);
+    AI ai(field);
+    (*field)[0] = player::AI;
+    (*field)[1] = player::AI;
+    (*field)[9] = player::AI;
+    (*field)[4] = player::Human;
     (*field)[5] = player::Human;
     (*field)[6] = player::Human;
-    (*field)[7] = player::Human;
     (*field)[8] = player::Human;
     std::pair<int, int> choice = ai.findBestMove();
-    EXPECT_EQ(choice.second, 9);
+    EXPECT_EQ(choice.second, 7);
 }
 
 void playEveryBoard(AI ai, std::shared_ptr<Field> f)
@@ -109,17 +111,23 @@ void playEveryBoard(AI ai, std::shared_ptr<Field> f)
         if (it == f->end())
             break;
         *it = player::Human;
-        //if (g.hasWon())
-        //    std::cout << std::endl;
-        EXPECT_TRUE(!g.hasWon());
+        if (f->hasWon())
+            std::cout << std::endl;
+        EXPECT_TRUE(!f->hasWon());
 
         bestMove = ai.findBestMove().second;
         if (bestMove == -1)//TIE
+        {
+            *it = player::None;
             break;
-
+        }
         (*f)[bestMove] = player::AI;
         if (f->hasWon())//AI WIN
-           break;
+        {
+            *it = player::None;
+            (*f)[bestMove] = player::None;
+            break;
+        }
 
         playEveryBoard(ai, f);
 
@@ -132,7 +140,7 @@ void playEveryBoard(AI ai, std::shared_ptr<Field> f)
 }
 TEST(AcceptanceTest, EveryBoard)
 {
-    std::shared_ptr<Field> field = std::make_shared<Field>(3);
+    std::shared_ptr<Field> field = std::make_shared<Field>(3 * 3);
     AI ai(field);
-    playEveryBoard(g);
+    playEveryBoard(ai, field);
 }
