5a4c002fa52f108f81079b0acbc288c94302c6cc
SzymonOzog <szymon.ozog@gmail.com>
2020-06-02 09:18:06 +0200

    Reworked tests

--- TicTacToeTest/test.cpp
+++ TicTacToeTest/test.cpp
@@ -11,56 +11,71 @@ TEST(FieldTests, HasWonTest)
         {
             //check horizontally
             f.nullify();
-            f[0 + i * fieldSide] = p;
-            f[1 + i * fieldSide] = p;
-            f[2 + i * fieldSide] = p;
-            f[3 + i * fieldSide] = p;
-            f[4 + i * fieldSide] = p;
-            EXPECT_EQ(f.hasWon(), true);
+            f.set(0 + i * fieldSide ,p);
+            f.set(1 + i * fieldSide ,p);
+            f.set(2 + i * fieldSide ,p);
+            f.set(3 + i * fieldSide ,p);
+            f.set(4 + i * fieldSide ,p);
+            EXPECT_EQ(f.getState(), field::Won);
            // check veritically
             f.nullify();
-            f[0 + i] = p;
-            f[5 + i] = p;
-            f[10 + i] = p;
-            f[15 + i] = p;
-            f[20 + i] = p;
-            EXPECT_EQ(f.hasWon(), true);
+            f.set(0 + i, p);
+            f.set(5 + i, p);
+            f.set(10 + i, p);
+            f.set(15 + i, p);
+            f.set(20 + i, p);
+            EXPECT_EQ(f.getState(), field::Won);
         }
         //check diagonally
         f.nullify();
-        f[0] = p;
-        f[6] = p;
-        f[12] = p;
-        f[18] = p;
-        f[24] = p;
-        EXPECT_EQ(f.hasWon(), true);
+        f.set(0, p);
+        f.set(6, p);
+        f.set(12, p);
+        f.set(18, p);
+        f.set(24, p);
+        EXPECT_EQ(f.getState(), field::Won);
         //check second diagonal
         f.nullify();
-        f[4] = p;
-        f[8] = p;
-        f[12] = p;
-        f[16] = p;
-        f[20] = p;
-        EXPECT_EQ(f.hasWon(), true);
+        f.set(4, p);
+        f.set(8, p);
+        f.set(12, p);
+        f.set(16, p);
+        f.set(20, p);
+        EXPECT_EQ(f.getState(), field::Won);
     }
 }
 
 TEST(FieldTests, isDrawTest)
 {
     Field field(5 * 5);
-    field[0] = player::AI;
-    field[4] = player::Human;
-    field[6] = player::Human;
-    field[8] = player::AI;
-    field[10] = player::Human;
-    field[12] = player::AI;
-    field[16] = player::AI;
-    field[18] = player::Human;
-    field[22] = player::Human;
-    field[24] = player::AI;
-    EXPECT_TRUE(field.isDraw());
-    field[24] = player::None;
-    EXPECT_TRUE(!field.isDraw());
+    field.set(0, player::AI);
+    field.set(4, player::Human);
+    field.set(6, player::Human);
+    field.set(8, player::AI);
+    field.set(10, player::Human);
+    field.set(12, player::AI);
+    field.set(16, player::AI);
+    field.set(18, player::Human);
+    field.set(22, player::Human);
+    field.set(24, player::AI);
+    EXPECT_EQ(field.getState(), field::Draw);
+    field.set(24, player::None);
+    EXPECT_EQ(field.getState(), field::None);
+}
+
+TEST(FieldTest, fullFieldWin)
+{
+    Field field(3 * 3);
+    field.set(1, player::AI);
+    field.set(2, player::AI);
+    field.set(3, player::AI);
+    field.set(4, player::Human);
+    field.set(5, player::Human);
+    field.set(6, player::AI);
+    field.set(7, player::Human);
+    field.set(8, player::AI);
+    field.set(9, player::Human);
+    EXPECT_EQ(field.getState(), field::Won);
 }
 //Not really a test, just checking how long will the AI think 
 //when provided with an empty field
